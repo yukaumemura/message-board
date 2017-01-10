5e7ba9c288f7ae452e5530730ab14bc2359383c8
YUKA Umemura <jackbaisonrunashaf@yahoo.co.jp>
2017-01-10 12:04:55 +0000

    first commit

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,25 @@
+class MessagesController < ApplicationController
+  def index
+    @message = Message.new
+    # Messageを全て取得する。
+    @messages = Message.all
+  end
+  
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      # メッセージが保存できなかった時
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  ## ここまで
+end

f052611f65c2975ec7a4f1e78178ed2b2aabc058
YUKA Umemura <jackbaisonrunashaf@yahoo.co.jp>
2017-01-16 11:53:38 +0000

    Gemfile update for heroku

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,6 @@ class Message < ActiveRecord::Base
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
   # 年齢は必須入力かつ0以上かつ150以下の数字  
-  validates :age , length: { minimum: 0, maximum: 150 } , presence: true
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 150 }
 end
+# テスト

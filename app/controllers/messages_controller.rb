3d98fa2b7aca9f4721cfe0e30b45416a115ae988
YUKA Umemura <jackbaisonrunashaf@yahoo.co.jp>
2017-01-16 10:13:13 +0000

    commit message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -38,7 +38,7 @@ def destroy
 
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body,:age)
   end
   
    def set_message
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'messages#index'
-  resources :messages , except: [:index, :new]
+  resources :messages , except: [:index, :new,:age]
 end
